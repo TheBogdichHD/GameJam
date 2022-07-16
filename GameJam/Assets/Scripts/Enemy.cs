6b9465bbcb28d52c8bc04a8501a960aa08c157a7
TheBogdichHD <nottoowellconstructed@gmail.com>
2022-07-16 19:23:45 +0300

    Enemy fix

--- GameJam/Assets/Scripts/Enemy.cs
+++ GameJam/Assets/Scripts/Enemy.cs
@@ -6,19 +6,36 @@ public class Enemy : MonoBehaviour
 {
     private Animator anim;
     public int cubeInt;
+    public ParticleSystem deathEffect;
+    private bool isDying;
+    private float deltaTime;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+        isDying = false;
+        deltaTime = 0;
     }
-
+    private void Update()
+    {
+        if (isDying)
+        {
+            deltaTime += Time.deltaTime;
+            if (deltaTime > 1.9f)
+            {
+                Destroy(gameObject);
+            }           
+        }
+    }
     private void OnTriggerStay2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
             if (coll.gameObject.GetComponent<Player>().currentInt >= cubeInt)
             {
+                deathEffect.Play();
                 anim.SetBool("isDeath", true);
+                isDying = true;
             }
             else
             {
