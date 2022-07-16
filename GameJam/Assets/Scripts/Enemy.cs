d3a0949bcf156c7d3a3518ccf66777035914bb5d
Анна Петросян <apetrosia@sfedu.ru>
2022-07-16 14:30:27 +0300

    Add enemy

---
+++ GameJam/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Animator anim;
+    public int cubeInt;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            if (coll.gameObject.GetComponent<Player>().currentInt >= cubeInt)
+            {
+                anim.SetBool("isDeath", true);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
